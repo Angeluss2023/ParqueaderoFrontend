2f395fe046e78abb4522863cdf0a4d0ef4a40d73
BPadillav <lissethpadil999@gmail.com>
2023-07-24 19:40:30 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './template/principal/principal.component';
+import { HistorialComponent } from './template/historial/historial.component';
+import { CostoComponent } from './template/costo/costo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "parqueo/registrar", component: PrincipalComponent},
+  {path: "parqueo/costo", component: CostoComponent},
+  {path: "parqueo/historial", component: HistorialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { PrincipalComponent } from './template/principal/principal.component';
+import { HistorialComponent } from './template/historial/historial.component';
+import { MenuComponent } from './template/menu/menu.component';
+import { CostoComponent } from './template/costo/costo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    HistorialComponent,
+    MenuComponent,
+    CostoComponent
   ],
   imports: [
     BrowserModule,
